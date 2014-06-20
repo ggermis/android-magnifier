15771f8433e10b1781ff304789bc2a0fc9eafa2a
Gerrit Germis <gerrit.germis@up-nxt.com>
2014-06-20 14:51:17 +0200

    remove dead code

--- src/org/codenut/app/magnifier/PreviewImage.java
+++ src/org/codenut/app/magnifier/PreviewImage.java
@@ -1,14 +1,7 @@
 package org.codenut.app.magnifier;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Rect;
 import android.graphics.YuvImage;
-import android.os.AsyncTask;
-import android.view.View;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
-import android.widget.ImageView;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -19,7 +12,6 @@ import java.util.Date;
 
 
 public class PreviewImage {
-    private static final int ANIMATION_DURATION = 2000;
     private File mDirectory;
     private String mName;
     private ByteArrayOutputStream mCapture;
@@ -71,71 +63,11 @@ public class PreviewImage {
         }
     }
 
-    public void preview(final ImageView container) {
-        new AsyncTask<Void, Void, Bitmap>() {
-            @Override
-            protected Bitmap doInBackground(Void... params) {
-                return asBitmap();
-            }
-
-            @Override
-            protected void onPostExecute(Bitmap bitmap) {
-                applyFadeEffect(container, bitmap);
-            }
-        }.execute();
-    }
-
     private String getName() {
         return mName;
     }
 
     private File getFullPath() {
         return new File(mDirectory, getName());
-    }
-
-    private Bitmap asBitmap() {
-        File imgFile = new File(mDirectory, getName());
-        return BitmapFactory.decodeFile(imgFile.getAbsolutePath());
-    }
-
-    private void applyFadeEffect(final ImageView container, final Bitmap bitmap) {
-        Animation fadeIn = new AlphaAnimation(0.00f, 1.00f);
-        fadeIn.setDuration(ANIMATION_DURATION);
-        fadeIn.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-            }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                fadeOut(container);
-            }
-        });
-        container.setImageBitmap(bitmap);
-        container.startAnimation(fadeIn);
-    }
-
-    private void fadeOut(final ImageView container) {
-        Animation fadeOut = new AlphaAnimation(1.00f, 0.00f);
-        fadeOut.setDuration(ANIMATION_DURATION);
-        fadeOut.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-            }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                container.setVisibility(View.GONE);
-            }
-        });
-        container.startAnimation(fadeOut);
     }
 }
